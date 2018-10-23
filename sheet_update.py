fa093f626db914f256c4544528b03379440b45ce
TrangDg <nktdang@gmail.com>
2018-10-23 08:40:55 -0700

    update with logging

--- sheet_update.py
+++ sheet_update.py
@@ -1,19 +1,24 @@
 import requests
 import csv
+import logging
+import os 
 
-def main():
+
+logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO').upper())
+	
+
+def read():
 	f = open("aat_update.csv", "r")
 	r = csv.reader(f)
 
 	next(r) #Skip header
-
+	
+	
 	id_list = [] #create a list of AAT ID
-
 	all_the_terms = []
-
+	
 	for row in r:
 		aat_id = row[1]
-
 		# Condition to check whether id has already existed
 		# If not, add id to the id_list and run the rest
 		if aat_id not in id_list:
@@ -21,10 +26,11 @@ def main():
 			url = "http://aat-web-services-staging.getty.edu/aat/" + aat_id + ".json"
 
 			try:
-				webUrl = requests.get(url, timeout=0.2)
-
+				webUrl = requests.get(url, timeout=1)
+				logging.info("URL works")
 			except: 
-				print ("URL is not found:", aat_id)
+				# print ("URL is not found:", aat_id)
+				logging.warning("CANNOT open URL: %s", aat_id)
 				continue
 
 			data = webUrl.json()
@@ -41,13 +47,13 @@ def main():
 	return all_the_terms
 		
 
-termList = main()
+termList = read()
 
 def aatUpdate():
-	f = open("aat_update.csv", "w")
+	f = open("aat_update.csv", "w", encoding='utf-8')
 	w = csv.writer(f)
-
 	w.writerow(["Portal term", "AAT ID"])
+	
 	w.writerows(termList)
 
 	f.close()
